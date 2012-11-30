8080cecb6315845dab23f8ed9e132e28f28ac10b
Holger Just <hjust@meine-er.de>
2012-11-30 17:56:04 +0100

    Add haproxy_crt LWRP to create concatenated SSL certificate files

---
+++ providers/crt.rb
@@ -0,0 +1,30 @@
+action :create do
+  files = [
+    new_resource.key,
+    new_resource.certificate,
+    new_resource.intermediates
+  ].flatten.compact
+
+  haproxy = new_resource.resources(:service => "haproxy")
+  template new_resource.path do
+    source "concat_files.erb"
+
+    owner new_resource.owner
+    group new_resource.group
+    mode new_resource.mode
+
+    variables :files => files
+    action :create
+
+    notifies :reload, haproxy
+  end
+end
+
+action :delete do
+  haproxy = new_resource.resources(:service => "haproxy")
+
+  file new_resource.path do
+    action :delete
+    notifies :reload, haproxy
+  end
+end
---
+++ resources/crt.rb
@@ -0,0 +1,12 @@
+actions :create, :delete
+default_action :create
+
+attribute :path, :kind_of => String, :name_attribute => true, :required => true
+
+attribute :key, :kind_of => String
+attribute :certificate, :kind_of => String
+attribute :intermediate, :kind_of => [ String, Array ]
+
+attribute :owner, :kind_of => String, :default => "root"
+attribute :group, :kind_of => String, :default => "root"
+attribute :mode, :kind_of => [ String, Integer ], :default => "0600"

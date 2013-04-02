58265c85234d65102c6fa2a6cc1bb8120215952d
Holger Just <hjust@meine-er.de>
2013-04-02 19:11:15 +0200

    Pass the cookbook name as string, not as symbol

--- providers/template.rb
+++ providers/template.rb
@@ -4,7 +4,7 @@ include HAProxy::Helpers
 action :create do
   template "#{node['haproxy']['dir']}/#{new_resource.template_prefix}.d/#{new_resource.name}.cfg" do
     source new_resource.template || "#{new_resource.template_prefix}_#{new_resource.name}.erb"
-    cookbook new_resource.cookbook || cookbook_name # the calling cookbook by default
+    cookbook new_resource.cookbook || cookbook_name.to_s # the calling cookbook by default
     variables new_resource.variables
 
     action :create

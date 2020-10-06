746fc480d703b45f6c851dc850928efd3d9bbddf
Holger Just <hello@holgerjust.de>
2020-10-06 13:57:26 +0200

    Bump cookbook version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hjust@meine-er.de"
 license "Apache 2.0"
 description "Installs HAProxy and provides configuration primitives"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.1"
+version "0.1.0"
 
 # Probably much more, but we still need to test that
 %w{ debian ubuntu }.each do |os|

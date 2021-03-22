a4340a05b370f5e6a85781c70008d900f93ee637
Holger Just <holger@plan.io>
2021-03-22 22:52:13 +0100

    Bump version to 0.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hjust@meine-er.de"
 license "Apache 2.0"
 description "Installs HAProxy and provides configuration primitives"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.1.3"
+version "0.1.4"
 
 # Probably much more, but we still need to test that
 %w{ debian ubuntu }.each do |os|
